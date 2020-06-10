054264467c6acf4eb93b3a895f40fe328e2d5d4b
Arudovwen <successahon@gmail.com>
2020-06-10 13:14:23 +0100

    update env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,5 +29,6 @@ class AppServiceProvider extends ServiceProvider
         if (App::environment('production')) {
             URL::forceScheme('https');
         }
+        $this->app['request']->server->set('HTTPS','on');
     }
 }
